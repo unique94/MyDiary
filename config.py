63b4ce8c2ef462b04198308ac2ff5388294fc155
ywy <yaowy@tju.edu.cn>
2017-07-08 16:27:10 +0800

    database_config

---
+++ config.py
@@ -0,0 +1,52 @@
+'''
+Configuration
+'''
+
+__author__ = 'Yao Weiyi'
+
+import config_server
+
+class Dict(dict):
+    '''
+    Simple dict but support access as x.y style.
+    '''
+    def __init__(self, names=(), values=(), **kw):
+        super(Dict, self).__init__(**kw)
+        for k, v in zip(names, values):
+            self[k] = v
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+def merge(defaults, override):
+    r = {}
+    for k, v in defaults.items():
+        if k in override:
+            if isinstance(v, dict):
+                r[k] = merge(v, override[k])
+            else:
+                r[k] = override[k]
+        else:
+            r[k] = v
+    return r
+
+def toDict(d):
+    D = Dict()
+    for k, v in d.items():
+        D[k] = toDict(v) if isinstance(v, dict) else v
+    return D
+
+configs = config_server.configs
+try:
+    import config_local
+    configs = merge(configs, config_local.configs)
+except ImportError:
+    pass
+
+configs = toDict(configs)
---
+++ config_server.py
@@ -0,0 +1,14 @@
+# config_server.py
+
+configs = {
+    'db':{
+        'host':'localhost',
+        'port': 3306,
+        'user':'www-data',
+        'password':'www-data',
+        'database':'awesome'
+    },
+    'session':{
+        'secret': 'AwEsOmE'
+    }
+}
